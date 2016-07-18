b203fc49549a767226ac3bedfef84df62692db26
CocoaWiki <cocoa@kiwki.us>
2016-07-18 22:19:03 +0900

    Add files via upload

---
+++ SkinLiberty.php
@@ -0,0 +1,39 @@
+<?php
+class SkinLiberty extends SkinTemplate {
+
+	public $skinname = 'liberty';
+	public $stylename = 'Liberty';
+	public $template = 'LibertyTemplate';
+
+    public function initPage( OutputPage $out ) {
+		parent::initPage( $out );
+		$out->addMeta( 'viewport', 'width=device-width, initial-scale=1, maximum-scale=1' );
+		$out->addMeta( 'description', 'KiwiWiki' );
+		$out->addMeta( 'keywords', 'wiki,KiwiWiki,키위위키,' . $this->getSkin()->getTitle() );
+		$out->addMeta('theme-color', '#AA7949');
+		$out->addMeta('twitter:card', 'summary');
+		$out->addMeta('twitter:site', '@kiwkius');
+		$out->addMeta('twitter:title', $this->getSkin()->getTitle() );
+		$out->addMeta('twitter:description', 'KiwiWiki');
+		$out->addMeta('twitter:creator', '@wikicocoa');
+		$out->addMeta('twitter:image', 'https://kiwki.us/kiwiki.png');
+		$out->addModuleScripts( array(
+		    'skins.liberty.bootstrap'
+		) );
+		$out->addModuleScripts( array(
+		    'skins.liberty.layoutjs'
+		) );
+    }
+
+	function setupSkinUserCss( OutputPage $out ) {
+		parent::setupSkinUserCss( $out );
+ 			$out->addHeadItem( 'font-awesome', '<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" />' );
+			$out->addHeadItem( 'google-ads', '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>' );
+		$out->addModuleStyles( array(
+			'skins.liberty.styles'
+		) );
+	}
+	function addToBodyAttributes( $out, &$bodyAttrs ) {
+		$bodyAttrs['class'] .= " Liberty width-size";
+    }
+}
