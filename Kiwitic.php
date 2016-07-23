aca54bc5dd4193e48060df6c32020a108d2fe4a1
Cocoa <cocoa@kiwki.us>
2016-07-24 00:21:15 +0900

    Update Kiwitic.php

--- Kiwitic.php
+++ Kiwitic.php
@@ -3,7 +3,7 @@
 if ( function_exists( 'wfLoadSkin' ) ) {
 	wfLoadSkin( 'Kiwitic' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
-	$wgMessagesDirs['Liberty'] = __DIR__ . '/i18n';
+	$wgMessagesDirs['Kiwitic'] = __DIR__ . '/i18n';
 	/* wfWarn(
 		'Deprecated PHP entry point used for MonoBook skin. Please use wfLoadSkin instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
